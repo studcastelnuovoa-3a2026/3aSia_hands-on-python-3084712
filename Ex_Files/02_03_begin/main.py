0eda9b82d6d2a3c8fc3a8f1ce0015d48f59bc767
studcastelnuovoa-3a2026 <stud.castelnuovoa@gdromagnosi.edu.it>
2026-02-26 09:58:34 +0000

    es

--- Ex_Files/02_03_begin/main.py
+++ Ex_Files/02_03_begin/main.py
@@ -1,10 +1,10 @@
-NAMES = ["John", "Paul", "George", "Ringo"]
+NAMES = ["Gio", "Paul", "George", "Ringo"]
 AGES = [20, 21, 22, 23]
 
-JOHN = NAMES[0]
+GIO = NAMES[0]
 PAUL = NAMES[1]
 
-JOHN_PAUL = NAMES[:2]
+GIO_PAUL = NAMES[:2]
 GEORGE_RINGO = NAMES[2:]
 REVERSE = NAMES[::-1]
 EVERY_OTHER = NAMES[::2]
@@ -13,6 +13,6 @@ print(sum(AGES))
 print(min(AGES))
 print(max(AGES))
 
-print(JOHN_PAUL)
+print(GIO_PAUL)
 print(GEORGE_RINGO)
 print(REVERSE)
